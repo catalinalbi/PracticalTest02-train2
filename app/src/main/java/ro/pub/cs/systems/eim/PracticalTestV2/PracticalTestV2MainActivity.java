98d076eb85654f5b3366de7157b1545dbd424d1b
catalinalbi <albicatalin636@yahoo.com>
2020-05-19 12:23:05 +0300

    init

---
+++ app/src/main/java/ro/pub/cs/systems/eim/PracticalTestV2/PracticalTestV2MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.PracticalTestV2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTestV2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practicaltestv2_main);
+    }
+}
